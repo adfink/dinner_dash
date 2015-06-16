cdf26809adb25eacf733feb5de0ee124e8e54828
brettgrigsby <gbrett@gmail.com>
2015-06-15 20:12:50 -0600

    add user login functionality

--- spec/features/user_login_spec.rb
+++ spec/features/user_login_spec.rb
@@ -6,11 +6,10 @@ describe "user login", type: :feature do
 
   before :each do
     @user = User.create(full_name:"Andrew Fink", email:"andrewfink@gmail.com", password:"password")
+    visit root_path
   end
 
   it "user can login" do
-    visit root_path
-
     click_link("login")
     expect(current_path).to eq(login_path)
     page.fill_in "Email", with: "andrewfink@gmail.com"
@@ -19,7 +18,15 @@ describe "user login", type: :feature do
 
     expect(current_path).to eq(root_path)
     assert page.has_content?("Farm to Table")
+  end
 
+  it "can't login without an email" do
+    click_link("login")
+    expect(current_path).to eq(login_path)
+    page.fill_in "Password", with: "password"
+    click_button "come on in, farmhand"
+
+    expect(current_path).to eq(login_path)
   end
 
 end
