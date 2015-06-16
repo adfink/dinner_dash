6d35b6c24affb62eda08882b206985479c2c79c7
Chelsea Worrel <chelseaworrel@gmail.com>
2015-06-16 16:56:25 -0600

    created orders table, created a join table for orders and items

--- app/models/item.rb
+++ app/models/item.rb
@@ -4,4 +4,6 @@ class Item < ActiveRecord::Base
   validates :description, presence: true
   validates :title, presence: true, uniqueness: true
   validates :price, numericality: { greater_than: 0 }
+  has_many :order_items
+  has_many :orders, through: :order_items
 end
