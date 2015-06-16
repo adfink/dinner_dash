f7910764a97b04a9a996aa26b1a26bbc2a3c054a
brettgrigsby <gbrett@gmail.com>
2015-06-15 22:56:29 -0600

    add category model spec

--- spec/models/category_spec.rb
+++ spec/models/category_spec.rb
@@ -1,5 +1,22 @@
-# require 'rails_helper'
-#
-# RSpec.describe Category, type: :model do
-#   pending "add some examples to (or delete) #{__FILE__}"
-# end
+require 'rails_helper'
+
+RSpec.describe Category, type: :model do
+  let(:category) { Category.create(name: "Cheese") }
+
+  it "is valid" do
+    expect(category).to be_valid
+  end
+
+  it "is invalid without a name" do
+    category.name = nil
+
+    expect(category).not_to be_valid
+  end
+
+  it "is invalid without a unique name" do
+    category
+    category2 = Category.create(name: "Cheese")
+
+    expect(category2).not_to be_valid
+  end
+end
