59b87439d4242dc85692ef0a1e6e8c4933a5588f
Chelsea Worrel <chelseaworrel@gmail.com>
2015-06-17 11:54:25 -0600

    fixed merge conflict

--- config/routes.rb
+++ config/routes.rb
@@ -2,13 +2,9 @@ Rails.application.routes.draw do
   root to: 'users#index'
   resources :users, only: [:index, :new, :create]
   resources :categories, only: [:index, :show]
-<<<<<<< HEAD
   resources :orders, only: [:show]
-  
-=======
   resources :items, only: [:show]
-
->>>>>>> master
+  
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
