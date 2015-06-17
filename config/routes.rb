da0d72337a32545b02f7751d0342acc4c3b924a2
Chelsea Worrel <chelseaworrel@gmail.com>
2015-06-16 18:44:52 -0600

    working on user profile page and links to orders

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root to: 'users#index'
   resources :users, only: [:index, :new, :create]
   resources :categories, only: [:index, :show]
-
+  resources :orders, only: [:show]
+  
   get '/login', to: 'sessions#new'
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy'
