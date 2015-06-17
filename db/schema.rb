c06ef2b5d4c90c672246bb71477f659884120fc2
brettgrigsby <gbrett@gmail.com>
2015-06-16 20:23:10 -0600

    category show page has links to item show pages

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150616233545) do
+ActiveRecord::Schema.define(version: 20150617020434) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,8 +36,9 @@ ActiveRecord::Schema.define(version: 20150616233545) do
     t.string   "title"
     t.string   "description"
     t.integer  "price"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
+    t.datetime "created_at",                                                                                            null: false
+    t.datetime "updated_at",                                                                                            null: false
+    t.string   "photo_url",   default: "http://cornucopia.org/cereal-scorecard/images/profiles/farm_to_table_logo.jpg"
   end
 
   create_table "order_items", force: :cascade do |t|
---
+++ spec/features/category_show_spec.rb
@@ -0,0 +1,28 @@
+require 'rails_helper'
+
+describe "categories index and show", type: :feature do
+  before :each do
+    @category = Category.create(name: "cupcakes")
+  end
+
+  it "index shows all categories with links" do
+    Category.create(name: "not cupcakes")
+    visit categories_path
+
+    assert page.has_content?("menus")
+    assert page.has_link?("cupcakes")
+    assert page.has_link?("not cupcakes")
+  end
+
+  it "is a link to its show page containing its items" do
+    @category.items.create(title: "red velvet", description: "it's red!", price: 300)
+    @category.items.create(title: "chocolate", description: "it's brown!", price: 200)
+    visit categories_path
+    click_link("cupcakes")
+
+    expect(current_path).to eq(category_path(@category))
+    expect(page).to have_content("cupcakes")
+    expect(page).to have_link("red velvet")
+    expect(page).to have_link("chocolate")
+  end
+end
