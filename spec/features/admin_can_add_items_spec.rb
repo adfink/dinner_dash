96a56a914839c8e924622114df71a1156f240f81
Andrew Fink <andrewfink8@gmail.com>
2015-06-18 08:27:20 -0600

    admin items path more fully tested

--- spec/features/admin_can_add_items_spec.rb
+++ spec/features/admin_can_add_items_spec.rb
@@ -5,12 +5,24 @@ describe "admin", type: :feature do
   before :each do
     @user = User.create(full_name:"Andrew Fink", email:"andrewfink@gmail.com", password:"password", role: 1)
     ApplicationController.any_instance.stub(:current_user).and_return(@user)
+    @item = Item.create(title: "chicken-leg", description: "yum", price: 40000)
+  end
+
+  it "can navigate to items index" do
+    visit admin_user_path(@user)
+
+    expect(current_path).to eq(admin_user_path(@user))
+    assert page.has_content?("admin")
+    assert page.has_link?("items")
+    click_link("items")
+
+    expect(current_path).to eq(admin_items_path)
   end
 
   it "can add items" do
     visit admin_items_path
     assert page.has_link?("add an item")
-    click_link "add item"
+    click_link "add an item"
 
     expect(current_path).to eq(new_admin_item_path)
     page.fill_in "title", with: "cornbread"
@@ -20,6 +32,17 @@ describe "admin", type: :feature do
     expect(current_path).to eq(admin_items_path)
     assert page.has_content?("cornbread")
 
+  end
+
+  it "can edit an item" do
+    visit admin_items_path
+    click_link "edit"
+    expect(current_path).to eq(edit_admin_item_path(@item))
+    page.fill_in "title", with: "big t-bone steak"
+    click_button "update this item"
+    expect(current_path).to eq(admin_items_path)
+    assert page.has_content?("big t-bone steak")
+    refute page.has_content?("meat")
   end
 
 end
