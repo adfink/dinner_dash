34abe3f6921374e9cbe13c30baeec72d4819473c
Andrew Fink <andrewfink8@gmail.com>
2015-06-22 14:22:45 -0600

    admin can now add menues to an item

--- app/controllers/admin/category_items_controller.rb
+++ app/controllers/admin/category_items_controller.rb
@@ -5,15 +5,20 @@ class Admin::CategoryItemsController < Admin::BaseController
     @items = Item.all
   end
 
+  def new_category_to_item
+    @categories = Category.all
+    @item = Item.find(params[:item_id])
+  end
+
 
   def create
     category_item = CategoryItem.new(category_item_params)
     if category_item.save
-      redirect_to admin_categories_path
+      redirect_to admin_user_path(current_user)
       # redirect_to admin_category_path(@category)
     else
       flash.now[:errors] = "oops that didn't persist to the database...as they say in the industry"
-      redirect_to admin_category_path(@category)
+      redirect_to admin_user_path(current_user)
     end
   end
 
