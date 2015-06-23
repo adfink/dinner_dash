cf9ebdbcb9ba80d8e18ddfccc53f9dd3945d79cf
Chelsea Worrel <chelseaworrel@gmail.com>
2015-06-22 21:06:45 -0600

    refactored admin items controller

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,5 +1,7 @@
 class Admin::ItemsController < Admin::BaseController
 
+before_action(:set_item, only: [:edit, :show, :update])
+
   def index
     @items = Item.all
   end
@@ -9,28 +11,25 @@ class Admin::ItemsController < Admin::BaseController
   end
 
   def edit
-    @item = Item.find(params[:id])
   end
 
   def show
-    @item = Item.find(params[:id])
   end
 
   def update
-    item = Item.find(params[:id])
-    item.update(item_params)
-    flash[:notice] = "Neato!!!...#{item.title} was just updated in our postgres database"
+    @item.update(item_params)
+    flash[:notice] = "Neato!!!...#{@item.title} was just updated in our postgres database"
     redirect_to admin_items_path
   end
 
   def create
-      item = Item.new(item_params)
-      if item.save
-        redirect_to admin_items_path
-      else
-        flash.now[:errors] = "oops that didn't persist to the database...as they say in the industry"
-        redirect_to new_admin_item_path
-      end
+    item = Item.new(item_params)
+    if item.save
+      redirect_to admin_items_path
+    else
+      flash.now[:errors] = "oops that didn't persist to the database...as they say in the industry"
+      redirect_to new_admin_item_path
+    end
   end
 
   private
@@ -39,4 +38,7 @@ class Admin::ItemsController < Admin::BaseController
     params.require(:item).permit(:title, :description, :price, :image)
   end
 
+  def set_item
+    @item = Item.find(params[:id])
+  end
 end
