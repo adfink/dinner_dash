ef769962bbc2668022c2f3b5a8eb560e964de751
brettgrigsby <gbrett@gmail.com>
2015-06-23 12:10:41 -0600

    add checkboxes to add or remove categories

--- app/models/item.rb
+++ app/models/item.rb
@@ -8,4 +8,8 @@ class Item < ActiveRecord::Base
   has_many :orders, through: :order_items
   has_attached_file :image, :default_url => "/images/default.jpg"
   validates_attachment_content_type :image, :content_type => [ "image/jpg", "image/jpeg", "image/png" ]
+
+  def part_of?(category)
+    categories.include?(category)
+  end
 end
