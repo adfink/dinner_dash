ac1ab85f3e503bd0be7c0e3332b61142749bdaec
Chelsea Worrel <chelseaworrel@gmail.com>
2015-06-23 08:47:06 -0600

    refactored models

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,11 +1,14 @@
 class Item < ActiveRecord::Base
   has_many :category_items
   has_many :categories, through: :category_items
+
   validates :description, presence: true
   validates :title, presence: true, uniqueness: true
   validates :price, numericality: { greater_than: 0 }
+
   has_many :order_items
   has_many :orders, through: :order_items
   has_attached_file :image, :default_url => "/images/default.jpg"
+
   validates_attachment_content_type :image, :content_type => [ "image/jpg", "image/jpeg", "image/png" ]
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,8 +1,11 @@
 class Order < ActiveRecord::Base
   belongs_to :user
+
   has_many :order_items
   has_many :items, through: :order_items
+
   enum status: { ordered: 0, paid: 1, cancelled: 2, completed: 3 }
+
   scope :ordered, -> { where(status: 0) }
   scope :paid, -> { where(status: 1) }
   scope :cancelled, -> { where(status: 2) }
