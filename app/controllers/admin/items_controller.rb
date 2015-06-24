5667232d1fb785c32df92b0124389b31a8b2d8e2
Andrew Fink <andrewfink8@gmail.com>
2015-06-24 14:05:18 -0600

    messin around with forms and shit

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -32,10 +32,11 @@ before_action(:set_item, only: [:edit, :show, :update])
 
   def create
     item = Item.new(item_params)
-    if item.save
+    if params[:category] && item.save
+      item.set_categories(params[:category][:ids])
       redirect_to admin_items_path
     else
-      flash.now[:errors] = "oops that didn't persist to the database...as they say in the industry"
+      flash[:errors] = "invalid item parameters"
       redirect_to new_admin_item_path
     end
   end
