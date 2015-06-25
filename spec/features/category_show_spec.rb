c99effb0d01c8ba2f44a50e91341fb248a172566
Andrew Fink <andrewfink8@gmail.com>
2015-06-25 10:37:38 -0600

    fixing some broke ass test

--- spec/features/category_show_spec.rb
+++ spec/features/category_show_spec.rb
@@ -5,20 +5,19 @@ describe "categories index and show", type: :feature do
     @category = Category.create(name: "cupcakes")
   end
 
-  it "index shows all categories with links" do
+  it "index shows all categories" do
     Category.create(name: "not cupcakes")
     visit categories_path
 
     assert page.has_content?("menus")
-    assert page.has_link?("cupcakes")
-    assert page.has_link?("not cupcakes")
+    expect(page).to have_css("#menulink")
   end
 
   it "is a link to its show page containing its items" do
     @category.items.create(title: "red velvet", description: "it's red!", price: 300)
     @category.items.create(title: "chocolate", description: "it's brown!", price: 200)
     visit categories_path
-    click_link("cupcakes")
+    find("#menulink").click
 
     expect(current_path).to eq(category_path(@category))
     expect(page).to have_content("cupcakes")
